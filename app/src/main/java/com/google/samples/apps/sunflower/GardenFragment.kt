967b82462645c2ba202a8dc05c4a0b8255b3acee
Manuel Lopez <manuel1n1@yahoo.com.mx>
2022-06-05 19:23:53 -0500

    -Menu item for searchView -SearchView filter plants by name -Clear filter in pause function -Stetics details with keyboard functionality

--- app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
@@ -17,15 +17,15 @@
 package com.google.samples.apps.sunflower
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.google.samples.apps.sunflower.adapters.GardenPlantingAdapter
 import com.google.samples.apps.sunflower.adapters.PLANT_LIST_PAGE_INDEX
 import com.google.samples.apps.sunflower.databinding.FragmentGardenBinding
+import com.google.samples.apps.sunflower.utilities.KeyBoard
 import com.google.samples.apps.sunflower.viewmodels.GardenPlantingListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,7 +50,51 @@ class GardenFragment : Fragment() {
         }
 
         subscribeUi(adapter, binding)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onPause() {
+        super.onPause()
+        with(viewModel){
+            clearPlantName()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_garden, menu)
+        val menuItem = menu.findItem(R.id.garden_search_bar)
+        if(menuItem != null) {
+            val keyboard = KeyBoard(context)
+            val searchView = menuItem.actionView as SearchView
+            searchView.setOnQueryTextFocusChangeListener { view, focus ->
+                if(focus)
+                    keyboard.showKeyboard()
+                else
+                    keyboard.hideKeyboard(view)
+            }
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return true
+                }
+                override fun onQueryTextChange(text: String?): Boolean {
+                    if (text != null)
+                        searchByName(text)
+                    return true
+                }
+            })
+            menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+                override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                    searchView.isIconified = false
+                    searchView.requestFocusFromTouch()
+                    return true
+                }
+                override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                    searchView.setQuery("", true)
+                    return true
+                }
+            })
+        }
     }
 
     private fun subscribeUi(adapter: GardenPlantingAdapter, binding: FragmentGardenBinding) {
@@ -67,5 +111,15 @@ class GardenFragment : Fragment() {
     private fun navigateToPlantListPage() {
         requireActivity().findViewById<ViewPager2>(R.id.view_pager).currentItem =
             PLANT_LIST_PAGE_INDEX
+    }
+
+    private fun searchByName(plantName: String) {
+        with(viewModel) {
+            if(plantName.isBlank()) {
+                clearPlantName()
+            } else {
+                setPlantName(plantName)
+            }
+        }
     }
 }
