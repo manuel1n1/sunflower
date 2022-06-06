ea3fe8d58e72b4f5582b2593cb4d573b932ce348
Manuel Lopez <manuel1n1@yahoo.com.mx>
2022-06-05 19:18:17 -0500

    -Implement new menu item for searchView -SearchView filter plants by name -Filter name is combine with grow zone -Clear filter name in pause function -Stetics details with keyboard functionality

--- app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
@@ -23,11 +23,13 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import com.google.samples.apps.sunflower.adapters.PlantAdapter
 import com.google.samples.apps.sunflower.databinding.FragmentPlantListBinding
+import com.google.samples.apps.sunflower.utilities.KeyBoard
 import com.google.samples.apps.sunflower.viewmodels.PlantListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -46,14 +48,53 @@ class PlantListFragment : Fragment() {
 
         val adapter = PlantAdapter()
         binding.plantList.adapter = adapter
-        subscribeUi(adapter)
+        subscribeUi(adapter, binding)
 
         setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onPause() {
+        super.onPause()
+        with(viewModel) {
+            clearPlantName()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_plant_list, menu)
+        val menuItem = menu.findItem(R.id.search_bar)
+        if(menuItem != null) {
+            val keyboard = KeyBoard(context)
+            val searchView = menuItem.actionView as SearchView
+            searchView.setOnQueryTextFocusChangeListener { view, focus ->
+                if(focus)
+                    keyboard.showKeyboard()
+                else
+                    keyboard.hideKeyboard(view)
+            }
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return true
+                }
+                override fun onQueryTextChange(text: String?): Boolean {
+                    if (text != null)
+                        searchByName(text)
+                    return true
+                }
+            })
+            menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+                override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                    searchView.isIconified = false
+                    searchView.requestFocusFromTouch()
+                    return true
+                }
+                override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                    searchView.setQuery("", true)
+                    return true
+                }
+            })
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -66,8 +107,9 @@ class PlantListFragment : Fragment() {
         }
     }
 
-    private fun subscribeUi(adapter: PlantAdapter) {
+    private fun subscribeUi(adapter: PlantAdapter, binding: FragmentPlantListBinding) {
         viewModel.plants.observe(viewLifecycleOwner) { plants ->
+            binding.hasResult = plants.isNotEmpty()
             adapter.submitList(plants)
         }
     }
@@ -78,6 +120,16 @@ class PlantListFragment : Fragment() {
                 clearGrowZoneNumber()
             } else {
                 setGrowZoneNumber(9)
+            }
+        }
+    }
+
+    private fun searchByName(plantName: String) {
+        with(viewModel) {
+            if(plantName.isBlank()) {
+                clearPlantName()
+            } else {
+                setPlantName(plantName)
             }
         }
     }
